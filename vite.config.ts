a765db9832e9973e4ff4da9bf7b04775984a74ef
sli39 <sliva391985@gmail.com>
2025-11-26 10:06:49 +0300

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/proba-4/' // имя репозитория на GitHub Pages
+  base: '/novij-4/' // имя репозитория на GitHub Pages
 })
